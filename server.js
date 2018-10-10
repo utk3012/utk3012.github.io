df847eeba3c8a740b7a4270399e6282994cf8425
Utkarsh Raj <utkarsh3012@gmail.com>
2018-10-10 15:06:25 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+app.use(express.static(path.join(__dirname)));
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join('/index.html'));
+});
+
+app.listen(8080);
